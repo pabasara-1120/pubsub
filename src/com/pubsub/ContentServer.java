8462fcd92eaf2dde8c0f2ead43ed3574fa6360b1
Pabasara Jayasekera <108865623+pabasara-1120@users.noreply.github.com>
2023-12-18 06:22:43 +0530

    Update ContentServer.java

--- src/com/pubsub/ContentServer.java
+++ src/com/pubsub/ContentServer.java
@@ -4,12 +4,15 @@ import java.util.Hashtable;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.ArrayList;
 
 public class ContentServer {
 	
-	private ArrayList<Subscriber> subcribersList;
+	private ArrayList<Subscriber> subscribersList;
 	private ExecutorService threadPool;
+	private final Lock subscribersLock;
 	
 	public static ContentServer serverInstance;
 	
@@ -21,21 +24,31 @@ public class ContentServer {
 	}
 	
 	private ContentServer() {
-		this.subcribersList = new ArrayList<>();
+		this.subscribersList = new ArrayList<>();
 		this.threadPool = Executors.newCachedThreadPool();
+		this.subscribersLock=new ReentrantLock();
 	}
 	
-	public void sendMessage(Message m) {
-		System.out.println("aaa");
-		for(Subscriber s: subcribersList) {
-			threadPool.execute(()->s.receivedMessage(m));
-			
-			
-		}
-		System.out.println("bbb");
-	}
+	public void sendMessage(Message message) {
+        subscribersLock.lock();
+        try {
+            for (Subscriber subscriber : subscribersList) {
+            	
+                threadPool.execute(() -> subscriber.receivedMessage(message));
+            }
+        } finally {
+            subscribersLock.unlock();
+        }
+    }
+	
+	
 	public void registerSubscriber(Subscriber s) {
-		subcribersList.add(s);
+		subscribersLock.lock();
+        try {
+            subscribersList.add(s);
+        } finally {
+            subscribersLock.unlock();
+        }
 	}
 	
 	public void shutdownThreadPool() {
